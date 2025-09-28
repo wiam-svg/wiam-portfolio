7e26667585545c43e4b04e03bb91187a6f1e5201
Wiam <aitelkadiwiam@gmail.com>
2025-09-28 19:32:21 +0200

    deploy portfolio_wiam

--- vite.config.ts
+++ vite.config.ts
@@ -33,5 +33,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "development" ? "/" : "/wiam-portfolio/",
+    base: "/",
 }));
